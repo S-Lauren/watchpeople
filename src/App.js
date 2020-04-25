75e187e47fc2306f73d92207b85671d5657b035e
S-Lauren <s.lauren.contact@gmail.com>
2020-04-25 14:51:48 +0200

    meta tags

--- src/App.js
+++ src/App.js
@@ -6,12 +6,7 @@ import MeinRouter from "./components/MeinRouter";
 function App() {
   return (
     <div className="App">
-      <MetaTags>
-            <title>Watch People</title>
-            <meta name="watch people" content="react app" />
-            <meta property="og:title" content="Watch People" />
-            <meta property="og:image" content="../public/watchpoeple_preview.png" />
-      </MetaTags>
+
       <MeinRouter /> 
     </div>
   );
