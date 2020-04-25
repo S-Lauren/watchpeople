35926cde36078c9a703c2832009ab6130d131d58
S-Lauren <s.lauren.contact@gmail.com>
2020-04-25 14:41:17 +0200

    try to add social preview image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
+import MetaTags from 'react-meta-tags';
 import './App.css';
 import MeinRouter from "./components/MeinRouter";
 function App() {
   return (
     <div className="App">
-           <MeinRouter /> 
+      <MetaTags>
+            <title>Watch People</title>
+            <meta name="watch people" content="react app" />
+            <meta property="og:title" content="Watch People" />
+            <meta property="og:image" content="../public/watchpoeple_preview.png" />
+      </MetaTags>
+      <MeinRouter /> 
     </div>
   );
 }
